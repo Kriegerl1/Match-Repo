1ad511bd7deb5ae81c456b1ca4728fec56a487af
Leonardo Rodrigues <lthkrieger@gmail.com>
2023-08-18 22:13:21 -0300

    Improve in responsitivity

--- assets/scroll-btn.js
+++ assets/scroll-btn.js
@@ -1,19 +1,25 @@
 const btnAnima = document.querySelector('#getTop');
 const Anchor = document.querySelector('#about-me');
+const AnchorEnd = document.querySelector('.footer')
 const screenView = window.innerHeight;
 
-console.log(screenView)
 
 function goTop() {
     const topoItem = Anchor.getBoundingClientRect().top;
+    const fimPagina = AnchorEnd.getBoundingClientRect().top;
+    const End = fimPagina - screenView <= 5;
     const visibleItem = topoItem - screenView < -800;
     // console.log(topoItem)
 
-    if (visibleItem){
+    if (visibleItem) {
         btnAnima.classList.add('show');
-    }else{
+    } else{
+        btnAnima.classList.remove('show')
+    } if (End) {
         btnAnima.classList.remove('show')
     }
 }
+
+
 
 window.addEventListener('scroll', goTop);
